3783e60f17459e306e8ff25c44c3b3d20c778d6e
oikwsat <oikwsat@gmail.com>
2021-07-22 13:40:28 +0900

    feat: create itemContainer

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}></View>
-        <View style={styles.rightContainer}></View>
+        <View style={styles.leftContainer}>
+          <Image style={{width: 100, height: 100,}} source={{uri: 'https://picsum.photos/id/10/200/200'}}/>
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text} numberOfLines={3}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Text>
+          <Text style={styles.subText}>React News</Text>
+        </View>
       </View>
     </View>
   );
-
 }
 
 const styles = StyleSheet.create({
@@ -26,14 +31,22 @@ const styles = StyleSheet.create({
     width: '100%',
     borderColor: 'gray',
     borderWidth: 1,
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   leftContainer: {
-    backgroundColor: 'red',
     width: 100,
   },
   rightContainer: {
-    backgroundColor: 'blue',
     flex: 1,
+    flexDirection: 'column',
+    padding: 10,
+    justifyContent: 'space-between',
   },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  }
 });
