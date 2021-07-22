fe8b5d4eb69060b684117df6d38c0b9ebdef5b32
oikwsat <oikwsat@gmail.com>
2021-07-22 15:23:30 +0900

    feat: ニュース記事の一覧表示

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import ListItem from './components/ListItem';
+import articles from './dummies/articles'
 
 export default function App() {
+  const items = articles.map((article, index) => {
+    return (
+      <ListItem
+        imageUrl={article.urlToImage}
+        title={article.title}
+        author={article.author}
+        key={index}
+      />
+    )
+  });
+
   return (
     <View style={styles.container}>
-      <ListItem
-        imageUrl='https://picsum.photos/id/10/200/200'
-        title='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-        author='SampleNews'
-      />
+      {items}
     </View>
   );
 }
