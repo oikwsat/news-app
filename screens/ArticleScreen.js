4e4861fffdd1fa7a5f7fc1bd3229805212e2788d
oikwsat <oikwsat@gmail.com>
2021-07-23 15:55:26 +0900

    feat: 画面遷移時にパラメータを渡す

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-export default function ArticleScreen() {
+export default function ArticleScreen({route}) {
+  const {article} = route.params;
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView source={{ uri: 'https://expo.io' }}/>
+      <WebView source={{ uri: article.url }}/>
     </SafeAreaView>
   );
 }
