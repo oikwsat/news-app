780fc94af3e6cf2e5eb2b9973761d03ae3010257
oikwsat <oikwsat@gmail.com>
2021-07-26 08:16:21 +0900

    feat: Reduxから状態を取得して画面に表示する

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import ListItem from '../components/ListItem';
 
-export default function ClipScreen() {
+export default function ClipScreen({navigation}) {
+  const user = useSelector(state => state.user);
+  const {clips} = user;
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+            <ListItem
+              imageUrl={item.urlToImage}
+              title={item.title}
+              author={item.author}
+              onPress={() => navigation.navigate('Article', {article: item})}
+            />
+          )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
     </SafeAreaView>
   );
 }
