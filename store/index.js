0924818ee42ae97f4172c0682bb274037d5e9976
oikwsat <oikwsat@gmail.com>
2021-07-27 09:54:04 +0900

    feat: AsyncStorageが非推奨なので推奨方式に変更

--- store/index.js
+++ store/index.js
@@ -2,7 +2,7 @@ import {combineReducers, createStore} from 'redux';
 import userReducer from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistReducer, persistStore } from 'redux-persist';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const rootReducer = combineReducers ({
   user: userReducer
